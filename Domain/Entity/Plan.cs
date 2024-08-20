9c6fbe9d195fb72cca90820d29fc220fd133e91f
Filipe Andrade <filipeandrade.work@gmail.com>
2024-08-20 14:47:49 -0300

    Criação da classe de Plan.cs que define o plano inteiro do aluno por um determinado periodo de tempo

---
+++ Domain/Entity/Plan.cs
@@ -0,0 +1,13 @@
+namespace Domain.Entity;
+
+public class Plan
+{
+    public Guid Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    
+    public DateTime StartDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public DateTime EndDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    
+    public List<Workout> workouts { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+}
