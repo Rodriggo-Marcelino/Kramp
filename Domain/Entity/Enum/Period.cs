f1eefc5bc7a779817a90eacb7c7116e59893fa7a
Filipe Andrade <filipeandrade.work@gmail.com>
2024-08-20 14:41:52 -0300

    Criação da classe de Workout e do Enum de Period que permite o usuário programar o dia que o treino se repetirá

---
+++ Domain/Entity/Enum/Period.cs
@@ -0,0 +1,20 @@
+namespace Domain.Entity.Enum;
+
+public enum Period
+{
+    DAILY = 1,
+    WEEKLY = 2,
+    
+    /*
+     * Variáveis JUMP
+     * Permitem ao criador do treino programar quantos dias de descanso até
+     * repetir o treino
+     * JUMP_ONE_DAY pula 1 dia (Treino Segunda, Pula Terça, Repete Quarta)
+     * JUMP_TWO_DAYS pula 2 dias (Treino Segunda, Pula Terça, Pula Quarta, Repete Quinta)
+     * JUMP_THREE_DAYS pula 3 dias (Treino Segunda, Pula Terça, Pula Quarta, Pula Quinta, Repete Sexta)
+     */
+    JUMP_ONE_DAY = 3,
+    JUMP_TWO_DAYS = 4,
+    JUMP_THREE_DAYS = 5,
+    
+}
---
+++ Domain/Entity/Workout.cs
@@ -0,0 +1,18 @@
+using Domain.Entity.Enum;
+
+namespace Domain.Entity;
+
+public class Workout
+{
+    public Guid Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string? Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string? Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    
+    public List<Muscle> TargetedMuscles { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    
+    public int SeriesCount { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int RepetitionCount { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    
+    public Period Period { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public DateTime CreatedAt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+}
