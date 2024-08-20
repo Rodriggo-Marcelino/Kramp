e89e7d04ade6790ee4e5fbc60f60f9abbaa63c21
Filipe Andrade <filipeandrade.work@gmail.com>
2024-08-20 15:40:38 -0300

    DataAnnotations para a classe de Address.cs

--- Domain/Entity/Address.cs
+++ Domain/Entity/Address.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,15 +8,21 @@ namespace Domain.Entity
 {
     public class Address
     {
+        [Key]
         public Guid Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        [Required]
         public string? Street { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        [Required]
         public string? PostalCode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        [Required]
         public string? Number { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public string? Complement { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        //Bairro
+        [Required]
         public string? District { get => throw new NotImplementedException(); set => throw new NotImplementedException(); } 
 
+        [Required]
         public string? City { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        [Required]
         public string? State { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
     }
