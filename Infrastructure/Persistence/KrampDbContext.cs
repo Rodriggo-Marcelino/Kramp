7fc3c54d6f1a5ba38d599047928a485b5bd24a61
Rodriggo Marcelino <rodriggo.eet@gmail.com>
2024-08-21 23:46:59 -0300

    Criação do DbContext

---
+++ Infrastructure/Persistence/KrampDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class KrampDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<Gym> Gyms { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Plan> Plans { get; set; }
+        public DbSet<Professional> Professionals { get; set; }
+        public DbSet<Workout> Workouts { get; set; }
+    }
+}
