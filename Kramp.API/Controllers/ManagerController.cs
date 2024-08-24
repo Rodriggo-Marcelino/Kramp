122364b795b8d0c0e58f20aab50eabe8aa9b82b2
Rodriggo Marcelino <rodriggo.eet@gmail.com>
2024-08-24 16:49:12 -0300

    Criação do controller

---
+++ Kramp.API/Controllers/ManagerController.cs
@@ -0,0 +1,22 @@
+﻿using Application.ManagerCQ.Commands;
+using Application.ManagerCQ.ViewModels;
+using Domain.Entity;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kramp.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ManagerController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpPost("Create-Manager")]
+        public async Task<ActionResult<ManagerInfoViewModel>> CreateManager(CreateManagerCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+    }
+}
