b56f0a2eef9211f059e6745b8c2ca6b6a2c66155
Rodriggo Marcelino <rodriggo.eet@gmail.com>
2024-08-24 16:44:35 -0300

    Criação do Command

---
+++ Application/ManagerCQ/Commands/CreateManagerCommand.cs
@@ -0,0 +1,18 @@
+﻿using Application.ManagerCQ.ViewModels;
+using MediatR;
+using Domain.Entity.Enum;
+
+namespace Application.ManagerCQ.Commands
+{
+    public record CreateManagerCommand : IRequest<ManagerInfoViewModel>
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? UserBio { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string? Username { get; set; }
+        public Document TypeDocument { get; set; }
+        public string? DocumentNumber { get; set; }
+        public string? Password { get; set; }
+    }
+}
