d44e5fcbcd560a0d57ef7f932f8d3c30a412ffa0
Rodriggo Marcelino <rodriggo.eet@gmail.com>
2024-08-24 16:45:03 -0300

    Criação da view model

---
+++ Application/ManagerCQ/ViewModels/ManagerInfoViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.ManagerCQ.ViewModels
+{
+    public record ManagerInfoViewModel
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? UserBio { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string? Username { get; set; }
+        public string? DocumentNumber { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+
+    }
+}
