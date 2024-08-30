34dcd54ef502a1697d9494a199939f2332de6acd
Rodriggo Marcelino <rodriggo.eet@gmail.com>
2024-08-30 18:02:24 -0300

    Adiciona registros ResponseBase e ResponseInfo
    Foram adicionados dois novos arquivos ao namespace `Application.Response`.
    O primeiro arquivo, `ResponseBase.cs`, define um registro genérico `ResponseBase<T>`
    que contém duas propriedades: `ResponseInfo?` e `T? Value`.
    O segundo arquivo, `ResponseInfo.cs`, define um registro `ResponseInfo`
    com três propriedades: `string? Title`, `string? ErrorDescription` e `int HttpStatus`.
    
    Essas mudanças introduzem um novo namespace `Application.Response` que contém
    dois novos registros. O registro `ResponseBase<T>` é um contêiner genérico para
    respostas, enquanto o registro `ResponseInfo` fornece informações detalhadas
    sobre a resposta, como título, descrição do erro e status HTTP.

---
+++ Application/Response/ResponseBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Response
+{
+    public record ResponseBase<T>
+    {
+        public ResponseInfo? ResponseInfo { get; init; }
+        public T? Value { get; init; }
+    }
+}
