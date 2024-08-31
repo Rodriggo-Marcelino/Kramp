4bc72466cdb534146e37cce9163aaeb1e6733b17
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-08-31 01:46:27 -0300

    Adiciona mapeamentos com AutoMapper e novos namespaces
    Foram adicionados novos namespaces: `Application.ManagerCQ.Commands`,
    `Application.ManagerCQ.Viewmodels`, `AutoMapper` e `Domain.Entity`.
    Foi criado o namespace `Application.Mapping` para organizar a nova
    classe de mapeamento `ProfileMappings`.
    
    A classe `ProfileMappings` herda de `Profile` e define dois mapeamentos
    no seu construtor:
    - De `CreateManagerCommand` para `Manager`, ignorando `Id`, `TypeDocument`
      e `PasswordHash`.
    - De `Manager` para `ManagerInfoViewModel`.

---
+++ Application/Mapping/ProfileMapping.cs
@@ -0,0 +1,20 @@
+﻿using Application.ManagerCQ.Commands;
+using Application.ManagerCQ.ViewModels;
+using AutoMapper;
+using Domain.Entity;
+
+namespace Application.Mapping
+{
+    public class ProfileMappings : Profile
+    {
+        public ProfileMappings()
+        {
+            CreateMap<CreateManagerCommand, Manager>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.TypeDocument, opt => opt.Ignore())
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore());
+
+            CreateMap<Manager, ManagerInfoViewModel>();
+        }
+    }
+}
