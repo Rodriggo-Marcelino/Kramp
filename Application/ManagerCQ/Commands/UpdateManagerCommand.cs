620b4c96f69936eca77d2ca9020a4205776239ec
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-08-31 17:05:08 -0300

    Implementação de UpdateManagerCommand
    - Adicionados novos using statements para `Application.ManagerCQ.ViewModels`, `Application.Response`, `MediatR` e `System.Text.Json.Serialization`.
    - Criado o namespace `Application.ManagerCQ.Commands`.
    - Implementada a classe `UpdateManagerCommand` herdando de `IRequest<ResponseBase<ManagerInfoViewModel>>`.
    - Adicionadas propriedades `Id` (com `JsonIgnore`), `Name`, `Surname`, `UserBio`, `BirthDate`, `Username` e `DocumentNumber` na classe `UpdateManagerCommand`.

---
+++ Application/ManagerCQ/Commands/UpdateManagerCommand.cs
@@ -0,0 +1,19 @@
+﻿using Application.ManagerCQ.ViewModels;
+using Application.Response;
+using MediatR;
+using System.Text.Json.Serialization;
+
+namespace Application.ManagerCQ.Commands
+{
+    public class UpdateManagerCommand : IRequest<ResponseBase<ManagerInfoViewModel>>
+    {
+        [JsonIgnore]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? UserBio { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string? Username { get; set; }
+        public string? DocumentNumber { get; set; }
+    }
+}
