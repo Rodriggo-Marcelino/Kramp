ef80cfe966b3fd43822f8f64bd59e55c66b972cd
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-08-31 16:38:02 -0300

    Requisições GET no ManagerController
    - Adicionadas dependências `AutoMapper` e `KrampDbContext` ao `ManagerController`.
    - Construtor modificado para injetar `KrampDbContext`, `IMapper` e `IMediator`.
    - Comentários adicionados sobre injeção temporária de `DbContext` e `Mapper`.
    - Método `Read` substituído por `GetAllManagers`.
    - Adicionado método `GetManagerById`.
    - Método `Update` agora aceita ID e `UpdateManagerCommand`.
    - Método `Delete` agora aceita ID.

--- Kramp.API/Controllers/ManagerController.cs
+++ Kramp.API/Controllers/ManagerController.cs
@@ -1,5 +1,8 @@
 ﻿using Application.ManagerCQ.Commands;
 using Application.ManagerCQ.ViewModels;
+using AutoMapper;
+using Domain.Entity;
+using Infrastructure.Persistence;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,9 +10,15 @@ namespace Kramp.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ManagerController(IMediator mediator) : ControllerBase
+    public class ManagerController(IMediator mediator, KrampDbContext context, IMapper mapper) : ControllerBase
     {
         private readonly IMediator _mediator = mediator;
+
+        // TODO: Repositories
+        // Injetando DbContext e Mapper temporariamente enquanto não temos repository
+        // Objetivo: Requisição GET
+        private readonly KrampDbContext _context = context;
+        private readonly IMapper _mapper = mapper;
 
         [HttpPost("Create")]
         public async Task<ActionResult<ManagerInfoViewModel>> Create(CreateManagerCommand command)
@@ -17,20 +26,30 @@ namespace Kramp.API.Controllers
             return Ok(await _mediator.Send(command));
         }
 
-        [HttpGet("Read")]
-        public async Task<ActionResult<ManagerInfoViewModel>> Read(CreateManagerCommand command)
+        [HttpGet("All")]
+        public async Task<ActionResult<IEnumerable<ManagerInfoViewModel>>> GetAllManagers()
+        {
+            List<Manager> managers = _context.Managers.ToList();
+            IEnumerable<ManagerInfoViewModel> response = _mapper.Map<IEnumerable<ManagerInfoViewModel>>(managers);
+            return Ok(response);
+        }
+
+        [HttpGet("{Id:guid}")]
+        public async Task<ActionResult<ManagerInfoViewModel>> GetManagerById(Guid Id)
+        {
+            Manager manager = _context.Managers.Find(Id);
+            ManagerInfoViewModel response = _mapper.Map<ManagerInfoViewModel>(manager);
+            return Ok(response);
+        }
+
+        [HttpPut("Update/{Id:guid}")]
+        public async Task<ActionResult<ManagerInfoViewModel>> Update(Guid Id, UpdateManagerCommand command)
         {
             throw new NotImplementedException();
         }
 
-        [HttpPut("Update")]
-        public async Task<ActionResult<ManagerInfoViewModel>> Update(CreateManagerCommand command)
-        {
-            throw new NotImplementedException();
-        }
-
-        [HttpDelete("Delete")]
-        public async Task<ActionResult<ManagerInfoViewModel>> Delete(CreateManagerCommand command)
+        [HttpDelete("Delete/{Id:guid}")]
+        public async Task<ActionResult<ManagerInfoViewModel>> Delete(Guid id)
         {
             throw new NotImplementedException();
         }
