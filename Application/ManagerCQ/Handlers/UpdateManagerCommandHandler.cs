0e13ef1fa7d3882bd09dd23181646a40346e7bc8
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-08-31 17:06:05 -0300

    Adicionar UpdateManagerCommandHandler para atualizar gerente
    Adição de um novo manipulador de comando `UpdateManagerCommandHandler` na namespace `Application.ManagerCQ.Handlers`.
    O manipulador implementa a interface `IRequestHandler<UpdateManagerCommand, ResponseBase<ManagerInfoViewModel>>`.
    O construtor do manipulador recebe um contexto de banco de dados `KrampDbContext` e um mapeador `IMapper`.
    O método `Handle` do manipulador:
    - Busca um gerente existente no banco de dados pelo `Id` fornecido no comando.
    - Lança uma exceção se o gerente não for encontrado.
    - Mapeia o gerente existente para um novo objeto `Manager` e atualiza suas propriedades com os dados do comando.
    - Gera um novo `RefreshToken` e define o tempo de expiração do token para seis meses a partir da data atual.
    - Atualiza o gerente no banco de dados e salva as alterações.
    - Retorna uma resposta contendo o `ManagerInfoViewModel` atualizado.

---
+++ Application/ManagerCQ/Handlers/UpdateManagerCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using Application.ManagerCQ.Commands;
+using Application.ManagerCQ.ViewModels;
+using Application.Response;
+using AutoMapper;
+using Domain.Entity;
+using Infrastructure.Persistence;
+using MediatR;
+
+namespace Application.ManagerCQ.Handlers
+{
+    public class UpdateManagerCommandHandler : IRequestHandler<UpdateManagerCommand, ResponseBase<ManagerInfoViewModel>>
+    {
+        private readonly KrampDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UpdateManagerCommandHandler(KrampDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<ResponseBase<ManagerInfoViewModel>> Handle(UpdateManagerCommand request,
+                                                       CancellationToken cancellationToken)
+        {
+            Manager oldManager = _context.Managers.FirstOrDefault(manager => manager.Id.Equals(request.Id));
+
+            if (oldManager == null)
+            {
+                throw new Exception("Manager not found.");
+            }
+
+            var newManager = _mapper.Map<Manager>(oldManager);
+            newManager = _mapper.Map(request, newManager);
+            newManager.RefreshToken = Guid.NewGuid().ToString();
+            newManager.RefreshTokenExpiryTime = DateTime.UtcNow.AddMonths(6);
+
+            _context.Managers.Update(newManager);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new ResponseBase<ManagerInfoViewModel>
+            {
+                ResponseInfo = null,
+                Value = _mapper.Map<ManagerInfoViewModel>(newManager)
+            };
+        }
+    }
+}
