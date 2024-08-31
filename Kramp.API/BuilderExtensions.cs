0486e2104b2f8148d115b5edb87deef9a24ebd2d
Rodriggo Marcelino <rodriggo.eet@gmail.com>
2024-08-30 22:15:23 -0300

    Refatora configuração em métodos de extensão
    Refatora a configuração de serviços, banco de dados e validações em uma classe de extensão separada chamada `BuilderExtensions`.
    
    - Adiciona métodos estáticos `AddServices`, `AddDatabase` e `AddValidations` em `BuilderExtensions.cs`.
    - Substitui chamadas diretas em `Program.cs` por métodos da nova classe `BuilderExtensions`.
    
    Isso resulta em um código mais limpo e modular, separando a lógica de configuração em métodos distintos e reutilizáveis.

---
+++ Kramp.API/BuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Application.ManagerCQ.Commands;
+using Application.ManagerCQ.Validators;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kramp.API
+{
+    public static class BuilderExtensions
+    {
+        public static void AddServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(typeof(CreateManagerCommand).Assembly));
+        }
+        public static void AddDatabase(this WebApplicationBuilder builder)
+        {
+            var configuration = builder.Configuration;
+            builder.Services.AddDbContext<KrampDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            // builder.Services.AddEntityFrameworkNpgsql().AddDbContext<KrampDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+            // "Server=localhost;Port=5432;Database=kramp;User Id=andrade;Password=Andrade*5432;"
+        }
+
+        public static void AddValidations(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddValidatorsFromAssemblyContaining<CreateManagerCommandValidator>();
+            builder.Services.AddFluentValidationAutoValidation();
+        }
+    }
+}
--- Kramp.API/Program.cs
+++ Kramp.API/Program.cs
@@ -1,30 +1,17 @@
-using Application.ManagerCQ.Commands;
-using Infrastructure.Persistence;
-using Microsoft.EntityFrameworkCore;
-using FluentValidation;
-using Application.ManagerCQ.Validators;
-using FluentValidation.AspNetCore;
+using Kramp.API;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<KrampDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-// builder.Services.AddEntityFrameworkNpgsql().AddDbContext<KrampDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
-// "Server=localhost;Port=5432;Database=kramp;User Id=andrade;Password=Andrade*5432;"
 
-builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(typeof(CreateManagerCommand).Assembly));
+builder.AddServices();
+builder.AddDatabase();
+builder.AddValidations();
 
-builder.Services.AddValidatorsFromAssemblyContaining<CreateManagerCommandValidator>();
-builder.Services.AddFluentValidationAutoValidation();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
