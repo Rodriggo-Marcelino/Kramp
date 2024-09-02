c3be1832d57820561e751cb831288c1bc56a3b01
Rodriggo Marcelino <rodriggo.eet@gmail.com>
2024-09-02 00:55:17 -0300

    Adição de IAuthService e melhorias no CreateManagerCommandHandler
    - Adicionada a dependência `IAuthService` na classe `CreateManagerCommandHandler`.
    - Modificado o construtor para aceitar `IAuthService`.
    - Alterado o método `Handle` para gerar o hash da senha (`PasswordHash`).
    - Substituído `SaveChangesAsync` por `SaveChanges` para salvar mudanças no banco.
    - Adicionada geração de token JWT (`TokenJWT`) e inclusão no `ManagerInfoViewModel`.
    - Removidos comentários TODO sobre validação de email, CPF e nome de usuário únicos, e criptografia da senha.

--- Application/ManagerCQ/Handlers/CreateManagerCommandHandler.cs
+++ Application/ManagerCQ/Handlers/CreateManagerCommandHandler.cs
@@ -2,6 +2,7 @@
 using Application.ManagerCQ.ViewModels;
 using Application.Response;
 using AutoMapper;
+using Domain.Abstractions;
 using Domain.Entity;
 using Infrastructure.Persistence;
 using MediatR;
@@ -10,37 +11,40 @@ namespace Application.ManagerCQ.Handlers
 {
     public class CreateManagerCommandHandler : IRequestHandler<CreateManagerCommand, ResponseBase<ManagerInfoViewModel?>>
     {
+        private readonly IAuthService _authService;
         private readonly KrampDbContext _context;
         private readonly IMapper _mapper;
 
-        public CreateManagerCommandHandler(KrampDbContext context, IMapper mapper)
+        public CreateManagerCommandHandler(IAuthService authService, KrampDbContext context, IMapper mapper)
         {
+            _authService = authService;
             _context = context;
             _mapper = mapper;
         }
 
-        public async Task<ResponseBase<ManagerInfoViewModel>> Handle(CreateManagerCommand request,
+        public async Task<ResponseBase<ManagerInfoViewModel?>> Handle(CreateManagerCommand request,
                                                        CancellationToken cancellationToken)
         {
             var manager = _mapper.Map<Manager>(request);
+
+         
             manager.PasswordHash = request.Password;
+
             manager.CreatedAt = DateTime.UtcNow;
             manager.RefreshToken = Guid.NewGuid().ToString();
             manager.RefreshTokenExpiryTime = DateTime.UtcNow.AddMonths(6);
 
             _context.Managers.Add(manager);
-            await _context.SaveChangesAsync(cancellationToken);
+            _context.SaveChanges();
 
-            return new ResponseBase<ManagerInfoViewModel>
+            var managerInfoVm = _mapper.Map<ManagerInfoViewModel>(manager);
+            managerInfoVm.TokenJWT = _authService.GenerateJWT(manager.DocumentNumber!, manager.Username!);
+
+            return new ResponseBase<ManagerInfoViewModel?>
             {
                 ResponseInfo = null,
-                Value = _mapper.Map<ManagerInfoViewModel>(manager)
+                Value = managerInfoVm
             };
         }
-
-        //TODO: Validação de email único
-        //TODO: Validação de CPF único
-        //TODO: Username unico
-        //TODO: Encriptar senha
     }
 }
