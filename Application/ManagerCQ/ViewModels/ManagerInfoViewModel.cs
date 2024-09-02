1866b81a2e790ea54871a17ef422c185e6db4c92
Rodriggo Marcelino <rodriggo.eet@gmail.com>
2024-09-02 00:55:29 -0300

    Adiciona propriedade TokenJWT em ManagerInfoViewModel
    Foi adicionada a propriedade `TokenJWT` do tipo `string?` na classe
    `ManagerInfoViewModel`. Esta propriedade será usada para armazenar
    um token JWT (JSON Web Token) associado ao usuário.

--- Application/ManagerCQ/ViewModels/ManagerInfoViewModel.cs
+++ Application/ManagerCQ/ViewModels/ManagerInfoViewModel.cs
@@ -9,6 +9,7 @@
         public string? Username { get; set; }
         public string? RefreshToken { get; set; }
         public DateTime? RefreshTokenExpiryTime { get; set; }
+        public string? TokenJWT { get; set; }
 
     }
 }
