7beb9d103db9754e27804ddd36f038caa403f53e
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-09-02 00:24:10 -0300

    alterando a inicialização da documentação para a nova versão

--- Kramp.API/Program.cs
+++ Kramp.API/Program.cs
@@ -17,9 +17,9 @@ var app = builder.Build();
 // Middleware para servir o arquivo JSON do Swagger
 app.Use(async (context, next) =>
 {
-    if (context.Request.Path.StartsWithSegments("/swagger/v1/v1_openapi.json"))
+    if (context.Request.Path.StartsWithSegments("/swagger/v1/V1.0.1_openapi.json"))
     {
-        var filePath = Path.Combine(AppContext.BaseDirectory, "Swagger", "v1_openapi.json");
+        var filePath = Path.Combine(AppContext.BaseDirectory, "Swagger", "V1.0.1_openapi.json");
         var jsonContent = await File.ReadAllTextAsync(filePath);
         context.Response.ContentType = "application/json";
         await context.Response.WriteAsync(jsonContent);
@@ -34,7 +34,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(options =>
-        options.SwaggerEndpoint("/swagger/v1/v1_openapi.json", "Kramp API v1")
+        options.SwaggerEndpoint("/swagger/v1/V1.0.1_openapi.json", "Kramp API v1")
     );
 }
 
