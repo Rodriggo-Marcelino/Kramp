8150268aa2cf54e32c56ca31298c625896802b15
Rodriggo Marcelino <rodriggo.eet@gmail.com>
2024-09-01 23:37:49 -0300

    Implementação do AuthService e geração de JWT
    - Adição de novas importações: `Domain.Abstractions`, `Microsoft.Extensions.Configuration` e `Microsoft.IdentityModel.Tokens`.
    - Criação do namespace `Services.AuthService`.
    - Implementação da classe `AuthService` que implementa `IAuthService`.
    - Construtor de `AuthService` inicializa `IConfiguration`.
    - Método `GenerateJWT` adicionado para gerar tokens JWT.
    - Recuperação do valor de `Issuer` a partir da configuração `Jwt`.

---
+++ Services/AuthService/AuthService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Abstractions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Services.AuthService
+{
+    public class AuthService(IConfiguration configuration) : IAuthService
+    {
+        private readonly IConfiguration _configuration = configuration;
+        public void GenerateJWT(string DocumentNumber, string password)
+        {
+
+            var issuer = _configuration["Jwt:Issuer"];
+
+        }
+    }
+}
