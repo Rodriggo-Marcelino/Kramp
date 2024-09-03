0661fee0ff2f804baa2732d738182b65e20f1e0e
Filipe Andrade <filipeandrade.work@gmail.com>
2024-09-03 15:46:39 -0300

    1- Adicionando ReponseBase em CreateMemberCommand.cs 2 - Adicionando o TokenJWT em MemberInfoViewModel.cs

--- Application/MemberCQ/ViewModels/MemberInfoViewModel.cs
+++ Application/MemberCQ/ViewModels/MemberInfoViewModel.cs
@@ -9,5 +9,6 @@
         public string? Username { get; set; }
         public string? RefreshToken { get; set; }
         public DateTime? RefreshTokenExpiryTime { get; set; }
+        public string? TokenJWT { get; set; }
     }
 }
