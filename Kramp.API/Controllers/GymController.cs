eafae5b4db049eda465ff6c09d3c332b3bbaa9b6
Filipe Andrade <filipeandrade.work@gmail.com>
2024-09-03 15:23:48 -0300

    implementando endpoints de Get e Delete em GymController.cs

--- Kramp.API/Controllers/GymController.cs
+++ Kramp.API/Controllers/GymController.cs
@@ -1,26 +1,41 @@
 ﻿using Application.GymCQ.Commands;
 using Application.GymCQ.ViewModels;
+using AutoMapper;
+using Domain.Entity;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Services.Repositories;
 
 namespace Kramp.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class GymController(IMediator mediator) : ControllerBase
+    public class GymController(IMediator _mediator, GymRepository _repository, IMapper _mapper) : ControllerBase
     {
-        private readonly IMediator _mediator = mediator;
-
         [HttpPost("Create")]
         public async Task<ActionResult<GymInfoViewModel>> Create(CreateGymCommand command)
         {
-            return Ok(await _mediator.Send(command));
+            return Created("" ,await _mediator.Send(command));
         }
 
-        [HttpGet("Read")]
-        public async Task<ActionResult<GymInfoViewModel>> Read(CreateGymCommand command)
+        [HttpGet("All")]
+        public async Task<ActionResult<GymInfoViewModel>> GetAllGyms()
         {
-            throw new NotImplementedException();
+            var gyms = await _repository.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<GymInfoViewModel>>(gyms));
+        }
+        
+        [HttpGet("{Id:guid}")]
+        public async Task<ActionResult<GymInfoViewModel>> GetGymById(Guid Id)
+        {
+            Gym? gym = await _repository.GetByIdAsync(Id);
+
+            if (gym == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<GymInfoViewModel>(gym));
         }
 
         [HttpPut("Update")]
@@ -29,10 +44,12 @@ namespace Kramp.API.Controllers
             throw new NotImplementedException();
         }
 
-        [HttpDelete("Delete")]
-        public async Task<ActionResult<GymInfoViewModel>> Delete(CreateGymCommand command)
+        [HttpDelete("Delete/{Id:guid}")]
+        public async Task<ActionResult<GymInfoViewModel>> Delete(Guid Id)
         {
-            throw new NotImplementedException();
+            //TODO: Retirar o método de delete do controller (má prática)
+            await _repository.DeleteByIdAsync(Id, new CancellationToken());
+            return NoContent();
         }
     }
 }
