7bcc778fa00814cc677618650a1aa279979d7816
Filipe Andrade <filipeandrade.work@gmail.com>
2024-09-03 13:58:41 -0300

    Refatorar interface genérica IRepository com métodos CRUD assíncronos
    - Adiciona a interface `IRepository` com métodos assíncronos para operações CRUD.
    - Inclui métodos para obter, adicionar, atualizar e deletar entidades.
    - Adiciona métodos para contagem e verificação de existência de entidades.

--- Domain/Repository/IRepository.cs
+++ Domain/Repository/IRepository.cs
@@ -1,12 +1,29 @@
 ﻿
 namespace Domain.Repository
 {
-    public interface IRepository<T> where T : class
+    public interface IRepository<T, Guid> where T : class
     {
+        //GET
         Task<IEnumerable<T>> GetAllAsync();
-        Task<T> GetByIdAsync(Guid id);
+        Task<T?> GetByIdAsync(Guid Id);
+        Task<IEnumerable<T>> FindAllByIdAsync(IEnumerable<Guid> Ids);
+        Task<IEnumerable<T>> FindAllAsync(Func<IQueryable<T>, IOrderedQueryable<T>> OrderBy);
+        Task<IEnumerable<T>> FindAllAsync(Func<IQueryable<T>, IOrderedQueryable<T>> OrderBy, int page, int pageSize);
+        Task<bool> ExistsById(Guid Id);
+        Task<int> Count();
+        
+        //POST
         Task<T> AddAsync(T entity);
+        
+        //PUT
         Task<T> UpdateAsync(T entity);
-        Task<T> DeleteAsync(Guid id);
+        
+        //DELETE
+        Task DeleteAsync(T entity);
+        Task DeleteByIdAsync(Guid Id);
+        Task DeleteAllByIdAsync(IEnumerable<Guid> Ids);
+        Task DeleteAllAsync();
+        Task DeleteAllAsync(IEnumerable<T> entities);
+
     }
 }
