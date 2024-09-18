2569b9f7cec83f425919e3627341a472a3ea5783
Filipe Andrade <filipeandrade.work@gmail.com>
2024-09-17 21:38:28 -0300

    criação dos commands de Create e Update de Workout

--- Application/TrainingCQRS/WorkoutCQ/Commands/UpdateWorkoutCommand.cs
+++ Application/TrainingCQRS/WorkoutCQ/Commands/UpdateWorkoutCommand.cs
@@ -1,4 +1,6 @@
 using System.Text.Json.Serialization;
+using Domain.Entity;
+using Domain.Entity.Enum;
 
 namespace Application.WorkoutCQ.Commands;
 
@@ -6,4 +8,8 @@ public class UpdateWorkoutCommand
 {
     [JsonIgnore]
     public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? Descritption {get; set;}
+    public List<Muscle>? TargetedMuscles { get; set; }
+    public ICollection<Exercise>? Exercises { get; set; }
 }
