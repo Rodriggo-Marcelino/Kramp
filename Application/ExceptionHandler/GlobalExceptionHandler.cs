7d3389b5a4a52e4fd880d88255fc48defcceb318
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-09-18 14:47:43 -0300

    Adiciona manipulador global de exceções
    Adiciona referência ao pacote Microsoft.AspNetCore.Diagnostics.Middleware no Application.csproj.
    Implementa a classe GlobalExceptionHandler em GlobalExceptionHandler.cs para lidar com exceções globais, incluindo métodos para registrar exceções e retornar respostas JSON detalhadas.

---
+++ Application/ExceptionHandler/GlobalExceptionHandler.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Application.ExceptionHandler
+{
+    public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
+    {
+        private const string UNHANDLED_EXCEPTION_MSG = "Unhandled exception has occurred.";
+
+        private static readonly JsonSerializerOptions SerializerOptions = new(JsonSerializerDefaults.Web)
+        {
+            Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+        };
+
+        public async ValueTask<bool> TryHandleAsync(
+            HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+
+            logger.LogError(exception, exception.Message);
+
+            var problemDetails = CreateProblemDetails(httpContext, exception);
+            var json = ToJson(problemDetails);
+
+            const string contentType = "application/problem+json";
+            httpContext.Response.ContentType = contentType;
+            await httpContext.Response.WriteAsync(json, cancellationToken);
+
+            return true;
+        }
+
+        private ProblemDetails CreateProblemDetails(in HttpContext httpContext, in Exception exception)
+        {
+            var errorCode = "GEH01";
+            var statusCode = httpContext.Response.StatusCode;
+            var reasonPhrase = ReasonPhrases.GetReasonPhrase(statusCode);
+
+            if (string.IsNullOrEmpty(reasonPhrase))
+            {
+                reasonPhrase = UNHANDLED_EXCEPTION_MSG;
+            }
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = statusCode,
+                Title = reasonPhrase,
+                Extensions =
+            {
+                [nameof(errorCode)] = errorCode
+            }
+            };
+
+            problemDetails.Detail = exception.ToString();
+            problemDetails.Extensions["traceId"] = httpContext.TraceIdentifier;
+            problemDetails.Extensions["data"] = exception.Data;
+
+            return problemDetails;
+        }
+
+        private string ToJson(in ProblemDetails problemDetails)
+        {
+            try
+            {
+                return JsonSerializer.Serialize(problemDetails, SerializerOptions);
+            }
+            catch (Exception ex)
+            {
+                const string msg = "An exception has occurred while serializing error to JSON";
+                logger.LogError(ex, msg);
+            }
+
+            return string.Empty;
+        }
+    }
+}
