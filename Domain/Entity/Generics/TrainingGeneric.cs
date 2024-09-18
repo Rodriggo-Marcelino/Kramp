f3de6090d222a351c91d154e1617e46526511b8e
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-09-18 14:49:28 -0300

    Retirando a Anotação [NotMapped] das Classes genéricas, pois o EntityFramework Resolve de outra forma

--- Domain/Entity/Generics/TrainingGeneric.cs
+++ Domain/Entity/Generics/TrainingGeneric.cs
@@ -1,23 +1,22 @@
+using Domain.Entity.Interfaces;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Domain.Entity.Interfaces;
 
 namespace Domain.Entity.Generics;
 
-[NotMapped]
 public class TrainingGeneric : ITraining
 {
     [Key]
     public Guid Id { get; set; } = Guid.NewGuid();
-    
+
     [Required]
     [StringLength(50, MinimumLength = 3)]
     [Column(TypeName = "varchar(50)")]
     public string? Name { get; set; }
-    
+
     [StringLength(240)]
     [Column(TypeName = "varchar(240)")]
     public string? Description { get; set; }
-    
+
     public DateTime CreatedAt { get; set; }
 }
--- Domain/Entity/Generics/UserGeneric.cs
+++ Domain/Entity/Generics/UserGeneric.cs
@@ -1,16 +1,15 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using Domain.Entity.Enum;
 using Domain.Entity.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entity.Generics;
 
-[NotMapped]
 public class UserGeneric : IUser
 {
     [Key]
     public Guid Id { get; set; } = Guid.NewGuid();
-    
+
     [Required]
     [StringLength(50, MinimumLength = 3)]
     [Column(TypeName = "varchar(50)")]
@@ -20,18 +19,18 @@ public class UserGeneric : IUser
     [StringLength(50, MinimumLength = 3)]
     [Column(TypeName = "varchar(50)")]
     public string? Username { get; set; }
-    
+
     [Required]
     public string? PasswordHash { get; set; }
 
     [Required]
     public Document TypeDocument { get; set; }
-    
+
     [Required]
     [StringLength(20)]
     public string? DocumentNumber { get; set; }
 
     public DateTime CreatedAt { get; set; }
-    public string? RefreshToken {get; set;}
-    public DateTime? RefreshTokenExpiryTime {get; set;}
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
 }
