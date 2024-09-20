e6715286fd9ce23364580e74e4d2d5905c73c555
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-09-20 14:41:01 -0300

    Refatoração e adição de novas propriedades DbSet
    - Alterada a linha de importação para `Domain.Entity.Generics`.
    - Propriedade `Exercises` removida e reintroduzida no final.
    - Propriedade `Plans` movida para nova posição.
    - Adicionadas propriedades `Users`, `Trainings` e `WorkoutExercises`.

--- Infrastructure/Persistence/KrampDbContext.cs
+++ Infrastructure/Persistence/KrampDbContext.cs
@@ -1,16 +1,20 @@
 ﻿using Domain.Entity;
+using Domain.Entity.Generics;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence
 {
     public class KrampDbContext(DbContextOptions options) : DbContext(options)
     {
-        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<UserGeneric> Users { get; set; }
         public DbSet<Gym> Gyms { get; set; }
         public DbSet<Manager> Managers { get; set; }
         public DbSet<Member> Members { get; set; }
-        public DbSet<Plan> Plans { get; set; }
         public DbSet<Professional> Professionals { get; set; }
+        public DbSet<TrainingGeneric> Trainings { get; set; }
+        public DbSet<Plan> Plans { get; set; }
         public DbSet<Workout> Workouts { get; set; }
+        public DbSet<WorkoutExercise> WorkoutExercises { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
     }
 }
