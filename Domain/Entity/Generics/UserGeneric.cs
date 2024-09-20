de64cfef3e1f0df3ea1d90a2c0f838d2b9803ba2
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-09-20 13:44:47 -0300

    Renomear tabela de "user" para "users" no banco de dados
    A tabela no banco de dados foi renomeada de "user" para "users" para evitar conflitos com palavras reservadas.
    
    - No arquivo `UserGeneric.cs`, a anotação `[Table("user")]` foi alterada para `[Table("users")]`.
    - No arquivo `20240920163830_user-para-users-pois-palavra-reservada.Designer.cs`, foi gerado um novo modelo de migração que reflete a mudança de nome da tabela de "user" para "users".
    - No arquivo `20240920163830_user-para-users-pois-palavra-reservada.cs`, foi criada uma migração que renomeia a tabela "user" para "users" e atualiza todas as chaves estrangeiras e índices que referenciam essa tabela.
    - No arquivo `KrampDbContextModelSnapshot.cs`, a tabela "user" foi renomeada para "users" no snapshot do modelo do banco de dados.

--- Domain/Entity/Generics/UserGeneric.cs
+++ Domain/Entity/Generics/UserGeneric.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entity.Generics;
 
-[Table("user")]
+[Table("users")]
 public class UserGeneric : IUser
 {
     [Key]
---
+++ Infrastructure/Migrations/20240920163830_user-para-users-pois-palavra-reservada.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class userparauserspoispalavrareservada : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_gym_user_Id",
+                table: "gym");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_manager_user_Id",
+                table: "manager");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_member_user_Id",
+                table: "member");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_professional_user_Id",
+                table: "professional");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_user_plan_PlanId",
+                table: "user");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_user_workout_WorkoutId",
+                table: "user");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_user",
+                table: "user");
+
+            migrationBuilder.RenameTable(
+                name: "user",
+                newName: "users");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_user_WorkoutId",
+                table: "users",
+                newName: "IX_users_WorkoutId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_user_PlanId",
+                table: "users",
+                newName: "IX_users_PlanId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_users",
+                table: "users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_gym_users_Id",
+                table: "gym",
+                column: "Id",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_manager_users_Id",
+                table: "manager",
+                column: "Id",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_member_users_Id",
+                table: "member",
+                column: "Id",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_professional_users_Id",
+                table: "professional",
+                column: "Id",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_users_plan_PlanId",
+                table: "users",
+                column: "PlanId",
+                principalTable: "plan",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_users_workout_WorkoutId",
+                table: "users",
+                column: "WorkoutId",
+                principalTable: "workout",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_gym_users_Id",
+                table: "gym");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_manager_users_Id",
+                table: "manager");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_member_users_Id",
+                table: "member");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_professional_users_Id",
+                table: "professional");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_users_plan_PlanId",
+                table: "users");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_users_workout_WorkoutId",
+                table: "users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_users",
+                table: "users");
+
+            migrationBuilder.RenameTable(
+                name: "users",
+                newName: "user");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_users_WorkoutId",
+                table: "user",
+                newName: "IX_user_WorkoutId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_users_PlanId",
+                table: "user",
+                newName: "IX_user_PlanId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_user",
+                table: "user",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_gym_user_Id",
+                table: "gym",
+                column: "Id",
+                principalTable: "user",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_manager_user_Id",
+                table: "manager",
+                column: "Id",
+                principalTable: "user",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_member_user_Id",
+                table: "member",
+                column: "Id",
+                principalTable: "user",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_professional_user_Id",
+                table: "professional",
+                column: "Id",
+                principalTable: "user",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_user_plan_PlanId",
+                table: "user",
+                column: "PlanId",
+                principalTable: "plan",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_user_workout_WorkoutId",
+                table: "user",
+                column: "WorkoutId",
+                principalTable: "workout",
+                principalColumn: "Id");
+        }
+    }
+}
