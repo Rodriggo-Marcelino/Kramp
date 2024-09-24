6fc7e84653991d717d2ab83b58c25f56ba9a8b93
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-09-24 12:24:36 -0300

    Atualiza caminho do arquivo Swagger para V1.3.1
    As mudanças atualizam o caminho do arquivo JSON do Swagger de "V1.3.0_openapi.json" para "V1.3.1_openapi.json". Isso é feito em dois lugares:
    1. No middleware que serve o arquivo JSON do Swagger, onde o caminho do arquivo e o nome do arquivo são atualizados.
    2. Na configuração do Swagger UI, onde o endpoint do Swagger é atualizado para apontar para a nova versão do arquivo JSON.
    
    Essas mudanças garantem que a aplicação utilize a versão mais recente do arquivo de especificação OpenAPI (V1.3.1), refletindo quaisquer atualizações na documentação da API na interface do Swagger UI.

--- Kramp.API/Program.cs
+++ Kramp.API/Program.cs
@@ -22,9 +22,9 @@ app.UseExceptionHandler();
 
 app.Use(async (context, next) =>
 {
-    if (context.Request.Path.StartsWithSegments("/swagger/v1/V1.3.0_openapi.json"))
+    if (context.Request.Path.StartsWithSegments("/swagger/v1/V1.3.1_openapi.json"))
     {
-        var filePath = Path.Combine(AppContext.BaseDirectory, "Swagger", "V1.3.0_openapi.json");
+        var filePath = Path.Combine(AppContext.BaseDirectory, "Swagger", "V1.3.1_openapi.json");
         var jsonContent = await File.ReadAllTextAsync(filePath);
         context.Response.ContentType = "application/json";
         await context.Response.WriteAsync(jsonContent);
@@ -40,7 +40,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(options =>
-        options.SwaggerEndpoint("/swagger/v1/V1.3.0_openapi.json", "Kramp API v1")
+        options.SwaggerEndpoint("/swagger/v1/V1.3.1_openapi.json", "Kramp API v1")
     );
 }
 
