7d73854c256ffd9cb03927a19900c8b36c3b6be4
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-09-24 15:08:16 -0300

    Refatoração e novos métodos nos controllers de Plan e Workout
    - Adicionados comentários detalhando os endpoints no `PlanController` e `WorkoutController`.
    - Alterado retorno dos métodos no `PlanController` para usar `SimplePlanViewModel`.
    - Adicionados métodos `CreateSimple` e `CreateComplete` no `WorkoutController`.
    - Adicionados métodos `GetAllSimpleWorkouts` e `GetAllCompleteWorkouts` no `WorkoutController`.
    - Alterado retorno dos métodos no `WorkoutController` para usar `SimpleWorkoutViewModel` ou `CompleteWorkoutViewModel`.
    - Adicionado método `AddExerciseToWorkout` no `WorkoutController`.

--- Kramp.API/Controllers/PlanController.cs
+++ Kramp.API/Controllers/PlanController.cs
@@ -12,8 +12,37 @@ namespace Kramp.API.Controllers;
 [ApiController]
 public class PlanController(IMediator _mediator, PlanRepository _repository, IMapper _mapper) : ControllerBase
 {
+    /*
+     * Lista de Endpoints Necessários para Plans:
+     * 
+     * -- Criar Plano --
+     * POST /api/plans (Cria um novo plano com informações básicas)
+     * POST /api/plans/complete (Cria um novo plano com detalhes completos, incluindo treinos)
+     * 
+     * -- Adicionar Treinos ao Plano --
+     * POST /api/plans/{planId}/workouts (Adiciona um treino a um plano existente)
+     * POST /api/plans/{planId}/workouts/list (Adiciona uma lista de treino a um plano especifico)
+     * 
+     * -- Ler Planos (Plans) --
+     * GET /api/plans (Retorna todos os planos cadastrados)
+     * GET /api/plans/{id} (Retorna um plano específico pelo ID)
+     * GET /api/plans/details (Retorna todos os planos com detalhes completos, incluindo treinos)
+     * GET /api/plans/{pageNumber}/{pageSize} (Retorna todos os planos cadastrados com paginação)
+     * GET /api/plans/details/{pageNumber}/{pageSize} (Retorna todos os planos cadastrados com paginação)
+     * GET /api/plans/{planId}/workouts (Retorna todos os treinos associados a um plano específico)
+     * 
+     * -- Atualizar Plano (Plans) --
+     * PUT /api/plans/{id} (Atualiza as informações de um plano específico)
+     * PUT /api/plans/{id}/workouts (Atualiza os workouts de um plano específico)
+     * 
+     * -- Remover Plano ou Treinos do Plano --
+     * DELETE /api/plans/{id} (Remove um plano específico do sistema)
+     * DELETE /api/plans/{planId}/workouts/{workoutId} (Remove um treino específico de um plano)
+     * DELETE /api/plans/{planId}/workouts (Remove todos os treinos de um plano específico)
+     */
+
     [HttpPost("Create")]
-    public async Task<ActionResult<PlanInfoViewModel>> Create(CreatePlanCommand command)
+    public async Task<ActionResult<SimplePlanViewModel>> Create(CreatePlanCommand command)
     {
         return Created("", await _mediator.Send(command));
     }
@@ -28,10 +57,10 @@ public class PlanController(IMediator _mediator, PlanRepository _repository, IMa
     }
 
     [HttpGet("All")]
-    public async Task<ActionResult<IEnumerable<PlanInfoViewModel>>> GetAllPlans()
+    public async Task<ActionResult<IEnumerable<SimplePlanViewModel>>> GetAllPlans()
     {
         var plans = await _repository.GetAllAsync();
-        return Ok(_mapper.Map<IEnumerable<PlanInfoViewModel>>(plans));
+        return Ok(_mapper.Map<IEnumerable<SimplePlanViewModel>>(plans));
     }
 
     /*
@@ -45,7 +74,7 @@ public class PlanController(IMediator _mediator, PlanRepository _repository, IMa
     */
 
     [HttpGet("{Id:guid}")]
-    public async Task<ActionResult<PlanInfoViewModel>> GetPlanById(Guid Id)
+    public async Task<ActionResult<SimplePlanViewModel>> GetPlanById(Guid Id)
     {
         Plan? plan = await _repository.GetByIdAsync(Id);
 
@@ -54,11 +83,11 @@ public class PlanController(IMediator _mediator, PlanRepository _repository, IMa
             return NotFound();
         }
 
-        return Ok(_mapper.Map<PlanInfoViewModel>(plan));
+        return Ok(_mapper.Map<SimplePlanViewModel>(plan));
     }
 
     [HttpPut("Update/{Id:guid}")]
-    public async Task<ActionResult<PlanInfoViewModel>> Update(Guid Id, UpdatePlanCommand command)
+    public async Task<ActionResult<SimplePlanViewModel>> Update(Guid Id, UpdatePlanCommand command)
     {
         command.Id = Id;
         return Ok(await _mediator.Send(command));
--- Kramp.API/Controllers/WorkoutController.cs
+++ Kramp.API/Controllers/WorkoutController.cs
@@ -12,17 +12,66 @@ namespace Kramp.API.Controllers;
 [ApiController]
 public class WorkoutController(IMediator _mediator, WorkoutRepository _repository, IMapper _mapper) : ControllerBase
 {
-    [HttpPost("Create")]
-    public async Task<ActionResult<WorkoutInfoViewModel>> Create(CreateWorkoutCommand command)
+    /*
+     * Lista de Endpoints Necessários para Workouts:
+     * 
+     * -- Criar Treino (Workouts) --
+     * POST /api/workouts (Cria um novo treino com informações básicas)
+     * POST /api/workouts/complete (Cria um novo treino com detalhes completos, incluindo exercícios)
+     * 
+     * -- Ler Treinos (Workouts) --
+     * GET /api/workouts (Retorna todos os treinos com informações básicas)
+     * GET /api/workouts/details (Retorna todos os treinos com detalhes completos, incluindo exercícios)
+     * GET /api/workouts/{id} (Retorna um treino específico pelo ID com informações básicas)
+     * GET /api/workouts/{id}/details (Retorna um treino específico com detalhes completos)
+     * GET /api/workouts/{id}/exercises (Retorna todos os exercícios de um treino específico)
+     * 
+     * -- Buscar Exercícios (Exercises) --
+     * GET /api/exercises (Retorna todos os exercícios disponíveis no sistema)
+     * GET /api/exercises/{exerciseId} (Retorna detalhes de um exercício específico)
+     * 
+     * -- Atualizar Treino (Workouts) --
+     * PUT /api/workouts/{id} (Atualiza as informações de um treino específico)
+     * PUT /api/workouts/{id}/exercises (Atualiza um ou mais exercícios de um treino específico)
+     * 
+     * -- Deletar Treino (Workouts) --
+     * DELETE /api/workouts/{id} (Exclui um treino específico)
+     * DELETE /api/workouts/{id}/exercises/{exerciseId} (Remove um exercício específico de um treino)
+     */
+
+    [HttpPost("Create/Simple")]
+    public async Task<ActionResult<SimpleWorkoutViewModel>> CreateSimple(CreateSimpleWorkoutCommand command)
     {
         return Created("", await _mediator.Send(command));
     }
 
-    [HttpGet("All")]
-    public async Task<ActionResult<IEnumerable<WorkoutInfoViewModel>>> GetAllWorkouts()
+    [HttpPost("Create/Complete")]
+    public async Task<ActionResult<CompleteWorkoutViewModel>> CreateComplete(CreateCompleteWorkoutCommand command)
+    {
+        return Created("", await _mediator.Send(command));
+    }
+
+    // TODO: Implementar os métodos abaixo
+    [HttpPost("{workoutId:guid}/Exercises")]
+    public async Task<IActionResult> AddExerciseToWorkout(Guid workoutId /*, AddExerciseToWorkoutCommand command*/)
+    {
+        //command.WorkoutId = workoutId;
+        // Lógica para adicionar o exercício ao Workout
+        return Ok();
+    }
+
+    [HttpGet("All/Simple")]
+    public async Task<ActionResult<IEnumerable<SimpleWorkoutViewModel>>> GetAllSimpleWorkouts()
     {
         var workouts = await _repository.GetAllAsync();
-        return Ok(_mapper.Map<IEnumerable<WorkoutInfoViewModel>>(workouts));
+        return Ok(_mapper.Map<IEnumerable<SimpleWorkoutViewModel>>(workouts));
+    }
+
+    [HttpGet("All/Complete")]
+    public async Task<ActionResult<IEnumerable<CompleteWorkoutViewModel>>> GetAllCompleteWorkouts()
+    {
+        var workouts = await _repository.GetAllAsync();
+        return Ok(_mapper.Map<IEnumerable<SimpleWorkoutViewModel>>(workouts));
     }
 
     /*
@@ -38,8 +87,8 @@ public class WorkoutController(IMediator _mediator, WorkoutRepository _repositor
     }
     */
 
-    [HttpGet("{Id:guid}")]
-    public async Task<ActionResult<WorkoutInfoViewModel>> GetWorkoutById(Guid Id)
+    [HttpGet("{Id:guid}/Simple")]
+    public async Task<ActionResult<SimpleWorkoutViewModel>> GetSimpleWorkoutById(Guid Id)
     {
         Workout? workout = await _repository.GetByIdAsync(Id);
 
@@ -48,27 +97,31 @@ public class WorkoutController(IMediator _mediator, WorkoutRepository _repositor
             return NotFound();
         }
 
-        return Ok(_mapper.Map<WorkoutInfoViewModel>(workout));
+        return Ok(_mapper.Map<SimpleWorkoutViewModel>(workout));
+    }
+
+    [HttpGet("{Id:guid}/Complete")]
+    public async Task<ActionResult<CompleteWorkoutViewModel>> GetCompleteWorkoutById(Guid Id)
+    {
+        Workout? workout = await _repository.GetByIdAsync(Id);
+
+        if (workout == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(_mapper.Map<SimpleWorkoutViewModel>(workout));
     }
 
     // TODO: Implementar os métodos abaixo
     [HttpGet("{Id:guid}/Exercises")]
-    public async Task<ActionResult<WorkoutInfoViewModel>> GetWorkoutExercisesById(Guid Id)
+    public async Task<ActionResult<SimpleWorkoutViewModel>> GetWorkoutExercisesById(Guid Id)
     {
         throw new NotImplementedException();
     }
 
-    // TODO: Implementar os métodos abaixo
-    [HttpPost("{workoutId:guid}/Exercises")]
-    public async Task<IActionResult> AddExerciseToWorkout(Guid workoutId /*, AddExerciseToWorkoutCommand command*/)
-    {
-        //command.WorkoutId = workoutId;
-        // Lógica para adicionar o exercício ao Workout
-        return Ok();
-    }
-
     [HttpPut("Update/{Id:guid}")]
-    public async Task<ActionResult<WorkoutInfoViewModel>> Update(Guid Id, UpdateWorkoutCommand command)
+    public async Task<ActionResult<SimpleWorkoutViewModel>> Update(Guid Id, UpdateSimpleWorkoutCommand command)
     {
         command.Id = Id;
         return Ok(await _mediator.Send(command));
