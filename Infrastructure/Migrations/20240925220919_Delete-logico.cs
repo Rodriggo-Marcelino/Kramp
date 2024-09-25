f329fc89e3b857f8dce121b2901b5e2a2d85d0fa
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-09-25 19:31:47 -0300

    Adiciona migrações e atualiza modelo do banco de dados
    - Adição de arquivos de migração `20240925220919_Delete-logico.Designer.cs` e `20240925220919_Delete-logico.cs` para adicionar colunas `CreatedAt`, `UpdatedAt` e `Deleted` em várias tabelas.
    - Modificação de `KrampDbContextModelSnapshot.cs` para refletir as mudanças no modelo do banco de dados e corrigir namespaces.
    - Alteração da coluna `TargetedMuscles` na tabela `workout` para permitir valores nulos.
    - Adição de novos relacionamentos entre entidades, incluindo `UserGeneric` com `Plan` e `Workout`, `PlanWorkout` com `Plan` e `Workout`, `WorkoutExercise` com `Exercise` e `Workout`, e entidades de usuário com `UserGeneric`.

---
+++ Infrastructure/Migrations/20240925220919_Delete-logico.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Deletelogico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "workout_exercise",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "workout_exercise",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "workout_exercise",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TargetedMuscles",
+                table: "workout",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "workout",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "training_plan",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "plan_workout",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "plan_workout",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "plan_workout",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "exercise",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "workout_exercise");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "workout_exercise");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "workout_exercise");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "workout");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "training_plan");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "plan_workout");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "plan_workout");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "plan_workout");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "exercise");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TargetedMuscles",
+                table: "workout",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
