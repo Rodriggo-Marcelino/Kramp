da12baf9ef5ec942c35b608eefb49d6ae519a636
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-09-27 20:23:10 -0300

    Refatoração e adição de novos templates de gerentes
    Foram adicionados novos `using` no arquivo `GetManagerByIdTemplate.cs` para incluir namespaces necessários, como `Application.CQRS.GenericsCQRS.Generic.Templates`, `Application.CQRS.GenericsCQRS.User.ViewModel`, `Application.CQRS.UsersCQRS.ManagerCQ.Queries`, `AutoMapper`, `Domain.Entity.User` e `Services.Repositories`.
    
    Foi criada uma nova classe `GetManagerByIdTemplate` no namespace `Application.CQRS.UsersCQRS.ManagerCQ.Templates`. Esta classe herda de `GetEntityByIdTemplate` e utiliza os tipos `Manager`, `GetManagerByIdQuery`, `UserGenericViewModel` e `ManagerRepository`. O construtor da classe inicializa a classe base com o repositório e o mapeador fornecidos.
    
    No arquivo `GetManagerTemplate.cs`, a classe `GetManagerTemplate` foi modificada para herdar de `GetAllEntitiesTemplate` em vez de `GetEntityTemplate`. Além disso, o tipo do segundo parâmetro genérico foi alterado de `GetManagerByIdQuery` para `GetAllManagersQuery`.

--- Application/CQRS/UsersCQRS/ManagerCQ/Templates/GetManagerByIdTemplate.cs
+++ Application/CQRS/UsersCQRS/ManagerCQ/Templates/GetManagerByIdTemplate.cs
@@ -7,9 +7,9 @@ using Services.Repositories;
 
 namespace Application.CQRS.UsersCQRS.ManagerCQ.Templates
 {
-    public class GetManagerTemplate : GetAllEntitiesTemplate<Manager, GetAllManagersQuery, UserGenericViewModel, ManagerRepository>
+    public class GetManagerByIdTemplate : GetEntityByIdTemplate<Manager, GetManagerByIdQuery, UserGenericViewModel, ManagerRepository>
     {
-        public GetManagerTemplate(ManagerRepository repository, IMapper mapper) : base(repository, mapper)
+        public GetManagerByIdTemplate(ManagerRepository repository, IMapper mapper) : base(repository, mapper)
         {
         }
     }
