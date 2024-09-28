7f771cc1088956575a1a60d234ab937fb0a7f5b4
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-09-28 16:09:52 -0300

    Adiciona DeleteManagerTemplate e corrige tipo genérico
    Adição da classe `DeleteManagerTemplate` no arquivo `DeleteManagerTemplate.cs`, que herda de `DeleteEntityTemplate` e é usada para deletar entidades do tipo `Manager` utilizando `DeleteEntityCommand<Manager>` e `ManagerRepository`.
    
    Correção de um erro tipográfico no arquivo `GetManagerByIdTemplate.cs`, onde o tipo genérico `GetEntitiesByIdQuery<UserGenericViewModel>` foi alterado para `GetEntityByIdQuery<UserGenericViewModel>` na definição da classe `GetManagerByIdTemplate`.

---
+++ Application/CQRS/UsersCQRS/ManagerCQ/Templates/DeleteManagerTemplate.cs
@@ -0,0 +1,14 @@
+﻿using Application.CQRS.GenericsCQRS.Generic.Commands;
+using Application.CQRS.GenericsCQRS.Generic.Templates;
+using Domain.Entity.User;
+using Services.Repositories;
+
+namespace Application.CQRS.UsersCQRS.ManagerCQ.Templates
+{
+    public class DeleteManagerTemplate : DeleteEntityTemplate<Manager, DeleteEntityCommand<Manager>, ManagerRepository>
+    {
+        public DeleteManagerTemplate(ManagerRepository repository) : base(repository)
+        {
+        }
+    }
+}
