394fb7fff5995ec1bba7030aeff0a645260147ee
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-09-28 17:49:12 -0300

    Simplificação de métodos e remoção de comentários
    Simplificação da função `ExecuteAsync` e métodos relacionados, removendo comentários desnecessários e blocos de código vazios. A lógica principal foi mantida, mas agora o código está mais conciso e fácil de manter.

--- Application/CQRS/GenericsCQRS/Generic/Templates/CreateEntityTemplate.cs
+++ Application/CQRS/GenericsCQRS/Generic/Templates/CreateEntityTemplate.cs
@@ -22,52 +22,34 @@ public abstract class CreateEntityTemplate<TEntity, TCommand, TViewModel, TRepos
 
     public async Task<ResponseBase<TViewModel>> ExecuteAsync(TCommand request)
     {
-        // Fase 1: Manipular o request antes de criar a entidade
         ManipulateRequest(request);
 
-        // Fase 2: Mapeia o comando para a entidade (pode ser personalizada)
         TEntity entity = MapCommandToEntity(request);
 
-        // Fase 5: Manipular a entidade após validação, mas antes de salvar
         ManipulateEntityBeforeSave(entity);
 
-        // Fase 6: Salvar a entidade no repositório
         await SaveEntityAsync(entity);
 
-        // Fase 7: Manipular a entidade após salvar (opcional)
         ManipulateEntityAfterSave(entity);
 
-        // Fase 8: Converter a entidade para o ViewModel e retornar
         return CreateResponse(entity);
     }
 
-    // Template Methods que podem ser sobrescritos para personalizar o comportamento
-
-    protected virtual void ManipulateRequest(TCommand request)
-    {
-        // Personalizar o comando antes da criação
-    }
+    protected virtual void ManipulateRequest(TCommand request) { }
 
     protected virtual TEntity MapCommandToEntity(TCommand request)
     {
-        // Lógica padrão de mapeamento (pode ser personalizada)
         return _mapper.Map<TEntity>(request);
     }
 
-    protected virtual void ManipulateEntityBeforeSave(TEntity entity)
-    {
-        // Personalizar a entidade antes de salvar (opcional)
-    }
+    protected virtual void ManipulateEntityBeforeSave(TEntity entity) { }
 
     protected virtual Task SaveEntityAsync(TEntity entity)
     {
         return _repository.AddAsync(entity);
     }
 
-    protected virtual void ManipulateEntityAfterSave(TEntity entity)
-    {
-        // Personalizar a entidade após salvar (opcional)
-    }
+    protected virtual void ManipulateEntityAfterSave(TEntity entity) { }
 
     protected virtual ResponseBase<TViewModel> CreateResponse(TEntity entity)
     {
