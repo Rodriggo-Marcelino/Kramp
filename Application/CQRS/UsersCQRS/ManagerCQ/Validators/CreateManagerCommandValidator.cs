c45d2b101dee86d8a99cde912c1b66d1f75e00e3
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-09-29 17:28:56 -0300

    Simplificação de validadores e ajustes de importações
    Simplificadas as classes `CreateManagerCommandValidator` e `UpdateManagerCommandValidator` ao remover parâmetros genéricos desnecessários, mantendo apenas `Manager` e os DTOs relevantes. Ajustadas as importações para refletir essas mudanças, removendo comandos genéricos e ViewModels não utilizados. Resultado: código mais limpo e fácil de manter.

--- Application/CQRS/UsersCQRS/ManagerCQ/Validators/CreateManagerCommandValidator.cs
+++ Application/CQRS/UsersCQRS/ManagerCQ/Validators/CreateManagerCommandValidator.cs
@@ -1,12 +1,10 @@
-﻿using Application.CQRS.GenericsCQRS.Generic.Commands;
-using Application.CQRS.GenericsCQRS.User.Commands;
+﻿using Application.CQRS.GenericsCQRS.User.Commands;
 using Application.CQRS.GenericsCQRS.User.Validators;
-using Application.CQRS.GenericsCQRS.User.ViewModel;
 using Domain.Entity.User;
 
 namespace Application.CQRS.UsersCQRS.ManagerCQ.Validators
 {
-    public class CreateManagerCommandValidator(ValidatorHelper helper) : CreateUserCommandValidator<Manager, CreateEntityCommand<Manager, CreateUserDTO, UserViewModel>, CreateUserDTO, UserViewModel>(helper)
+    public class CreateManagerCommandValidator(ValidatorHelper helper) : CreateUserCommandValidator<Manager, CreateUserDTO>(helper)
     {
     }
 }
--- Application/CQRS/UsersCQRS/ManagerCQ/Validators/UpdateManagerCommandValidator.cs
+++ Application/CQRS/UsersCQRS/ManagerCQ/Validators/UpdateManagerCommandValidator.cs
@@ -1,12 +1,10 @@
-﻿using Application.CQRS.GenericsCQRS.Generic.Commands;
-using Application.CQRS.GenericsCQRS.User.Commands;
+﻿using Application.CQRS.GenericsCQRS.User.Commands;
 using Application.CQRS.GenericsCQRS.User.Validators;
-using Application.CQRS.GenericsCQRS.User.ViewModel;
 using Domain.Entity.User;
 
 namespace Application.CQRS.UsersCQRS.ManagerCQ.Validators
 {
-    public class UpdateManagerCommandValidator(ValidatorHelper helper) : UpdateUserCommandValidator<Manager, UpdateEntityCommand<Manager, UpdateUserDTO, UserViewModel>, UpdateUserDTO, UserViewModel>(helper)
+    public class UpdateManagerCommandValidator(ValidatorHelper helper) : UpdateUserCommandValidator<Manager, UpdateUserDTO>(helper)
     {
     }
 }
