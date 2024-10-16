7354aa332188fcf5d227db03b100e1934c1c4c86
Filipe Andrade <filipeandrade.work@gmail.com>
2024-10-16 00:56:04 -0300

    feat: Adicionar Exercicio em workout
    Descoberta:
    1. Dá pra chamar o mediatr dentro de uma classe de handler sem problemas
    2. o EF entende que deve adicionar uma entidade que é filha no Json automaticamente, acredito que através de rastreamento.

--- Application/CQRS/Handlers/Create/AddExerciseToWorkoutHandler.cs
+++ Application/CQRS/Handlers/Create/AddExerciseToWorkoutHandler.cs
@@ -1,6 +1,26 @@
+using Application.CQRS.Commands.Create;
+using Application.CQRS.DTOs.Create;
+using Application.CQRS.Templates;
+using Application.CQRS.ViewModels;
+using AutoMapper;
+using Domain.Entity.Training;
+using Services.Repositories;
+
 namespace Application.CQRS.Handlers.Create;
 
-public class AddExerciseToWorkoutHandler
+public class AddExerciseToWorkoutHandler(WorkoutExerciseRepository repository, IMapper mapper)
+    : CreateEntityTemplate<
+        WorkoutExercise,
+        CreateEntityCommand<WorkoutExercise, AddExerciseToWorkoutDTO, WorkoutExerciseViewModel>,
+        AddExerciseToWorkoutDTO,
+        WorkoutExerciseViewModel,
+        WorkoutExerciseRepository>(repository, mapper)
 {
-
+    protected override void ManipulateEntityBeforeSave(IEnumerable<AddExerciseToWorkoutDTO> request, IEnumerable<WorkoutExercise> entities)
+    {
+        foreach (var entity in entities)
+        {
+            entity.CreatedAt = DateTime.UtcNow;
+        }
+    }
 }
